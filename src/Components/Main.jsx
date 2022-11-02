1d40ffc5edd0fd1c0b880db63312aea42504f766
Leandro Fernandes <leandrowebmaster@gmail.com>
2022-11-02 11:56:28 -0300

    updated: main and navbar content

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,15 +1,50 @@
 import React from "react";
 import * as RN from "react-native-web";
 
+import BigVan from '../assets/images/BigVan.png'
+
+
 export const Main = () => {
   return (
     <RN.View style={stlMain}>
-      <RN.Text>Main</RN.Text>
+      <RN.View>
+        <RN.View style={stlTxtView}>
+          <RN.Text>Locadora de Vans em São Paulo</RN.Text>
+        </RN.View>
+        <RN.View>
+          <RN.Text>
+            A Help Vans é especializada no transporte de passageiros. 
+
+            Com veículos confortáveis e sofisticados, buscamos sempre a excelência em nosso atendimento, se adequando a necessidade de nossos clientes fazendo assim um atendimento único e personalizado para cada situação. 
+
+            Além de contar com uma equipe treinada e veículos em ótimo estado, prezamos sempre pela qualidade de nossos serviços, além do conforto e segurança de nossos clientes!
+          </RN.Text>
+        </RN.View>
+      </RN.View>
+      <RN.View>
+        <RN.Image style={stlImg} source={BigVan} />
+      </RN.View>
     </RN.View>
   );
 };
 
 // ---------- set Styles
 const stlMain = {
-  flex: 1
+  flex: 1,
+  flexDirection: 'row'
 };
+
+const stlImg = {
+  width: 400,
+  height: 400,
+  // width: 400,
+  // height: 300,
+  // opacity: "100%",
+  // border: "solid",
+  // borderColor: "#0E0"
+};
+
+const stlTxtView = {
+  flexDirection: 'column',
+  width: "50%",
+}
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,84 @@
 import React from "react";
 import * as RN from "react-native-web";
+// import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import HpLogo3 from '../assets/images/HpLogo3.png'
+
 
 export const Navbar = () => {
+  const fnHome = () => { };
+  const fnAboutUs = () => { };
+  const fnServices = () => { };
+  const fnContact = () => { };
+
   return (
     <RN.View style={stlMain}>
-      <RN.Text>Navbar</RN.Text>
+      <RN.View style={stlBackground}>
+      <RN.Image style={stlImg} source={HpLogo3} />
+      <RN.View style={stlButtonsBox}>
+        <RN.TouchableOpacity style={stlButtons} onPress={fnHome}>
+          <RN.Text style={stlBtnTxt}>Home</RN.Text>
+          </RN.TouchableOpacity>
+        
+        <RN.TouchableOpacity style={stlButtons} onPress={fnAboutUs}>
+          <RN.Text style={stlBtnTxt}>Empresa</RN.Text>
+          </RN.TouchableOpacity>
+        
+        <RN.TouchableOpacity style={stlButtons} onPress={fnServices}>
+          <RN.Text style={stlBtnTxt}>Serviços</RN.Text>
+          </RN.TouchableOpacity>
+          
+        <RN.TouchableOpacity style={stlButtons} onPress={fnContact}>
+          <RN.Text style={stlBtnTxt}>Contato</RN.Text>
+          </RN.TouchableOpacity>
+      </RN.View>
+      </RN.View>
     </RN.View>
   );
 };
 
 // ---------- set Styles
 const stlMain = {
-  flex: 1
+  flex: 1,
+  width: '100%',
 };
+
+const stlBackground = {
+  width: '100%',
+  backgroundColor: '#000',
+  padding: 20,
+  flexDirection: 'row',
+};
+
+
+
+const stlImg = {
+      width: 341,
+      height: 156,
+      // width: 400,
+      // height: 300,
+      // opacity: "100%",
+      // border: "solid",
+      // borderColor: "#0E0"
+    };
+
+    const stlButtonsBox = {
+      flexDirection: 'row',
+      width: '100%',
+      height: '100%'
+    }
+
+    const stlButtons = {
+      backgroundColor: '#ccc',
+      padding: 4,
+      margin: 2,
+      borderRadius: '6px',
+
+    }
+
+    const stlBtnTxt = {
+      fontFamily: 'Roboto',
+      fontSize: 20,
+      fontStyle: 'normal',
+      // color: '#FFF'
+    }
